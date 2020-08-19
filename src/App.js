3d073615167b812211c6994ce06d42f4c586286b
Saad <saad_anwer@live.com>
2020-08-19 07:57:45 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      
+      <div className="app_header">
+        <div className="app_headerContent">
+        <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
+        alt=""
+        className="app_headerImage"
+      />
+        </div>
+     
+      </div>
     </div>
   );
 }
